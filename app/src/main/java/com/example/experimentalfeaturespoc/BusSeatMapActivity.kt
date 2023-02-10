099fb2de5384f6ce86e23a719317ad6a3a18b03a
yogigorle <gorleyogi@gmail.com>
2023-02-10 18:01:16 +0530

    working on bus seatmap activity

--- app/src/main/java/com/example/experimentalfeaturespoc/BusSeatMapActivity.kt
+++ app/src/main/java/com/example/experimentalfeaturespoc/BusSeatMapActivity.kt
@@ -10,6 +10,7 @@ import android.widget.GridLayout
 import android.widget.LinearLayout
 import android.widget.Space
 import androidx.appcompat.app.WindowDecorActionBar.TabImpl
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.ui.unit.dp
 import androidx.core.view.isVisible
 import androidx.core.view.setMargins
@@ -40,79 +41,79 @@ class BusSeatMapActivity : AppCompatActivity() {
         val totalCols = 12
         val dividerRow = 2
         val lowerDeckSeaterSeatsList = listOf(
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("4", 1, 1),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("8", 1, 2),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("12", 1, 3),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("16", 1, 4),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("20", 1, 5),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("24", 1, 6),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("28", 1, 7),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("32", 1, 8),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("36", 1, 9),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("40", 1, 10),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("44", 1, 11),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("48", 1, 12),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("53", 1, 13),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("3", 2, 1),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("7", 2, 2),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("11", 2, 3),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("15", 2, 4),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("19", 2, 5),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("23", 2, 6),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("26", 2, 7),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("31", 2, 8),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("35", 2, 9),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("39", 2, 10),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("43", 2, 11),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("47", 2, 12),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("52", 2, 13),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("51", 3, 13),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("2", 4, 1),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("6", 4, 2),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("10", 4, 3),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("14", 4, 4),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("18", 4, 5),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("22", 4, 6),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("26", 4, 7),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("30", 4, 8),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("34", 4, 9),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("38", 4, 10),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("42", 4, 11),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("46", 4, 12),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("50", 4, 13),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("1", 5, 1),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("5", 5, 2),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("9", 5, 3),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("13", 5, 4),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("17", 5, 5),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("21", 5, 6),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("25", 5, 7),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("29", 5, 8),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("33", 5, 9),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("37", 5, 10),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("41", 5, 11),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("45", 5, 12),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("49", 5, 13),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("4", 1, 1, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("8", 1, 2, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("12", 1, 3, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("16", 1, 4, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("20", 1, 5, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("24", 1, 6, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("28", 1, 7, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("32", 1, 8, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("36", 1, 9, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("40", 1, 10, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("44", 1, 11, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("48", 1, 12, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("53", 1, 13, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("3", 2, 1, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("7", 2, 2, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("11", 2, 3, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("15", 2, 4, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("19", 2, 5, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("23", 2, 6, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("26", 2, 7, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("31", 2, 8, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("35", 2, 9, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("39", 2, 10, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("43", 2, 11, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("47", 2, 12, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("52", 2, 13, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("51", 3, 13, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("2", 4, 1, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("6", 4, 2, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("10", 4, 3, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("14", 4, 4, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("18", 4, 5, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("22", 4, 6, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("26", 4, 7, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("30", 4, 8, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("34", 4, 9, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("38", 4, 10, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("42", 4, 11, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("46", 4, 12, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("50", 4, 13, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("1", 5, 1, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("5", 5, 2, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("9", 5, 3, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("13", 5, 4, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("17", 5, 5, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("21", 5, 6, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("25", 5, 7, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("29", 5, 8, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("33", 5, 9, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("37", 5, 10, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("41", 5, 11, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("45", 5, 12, "SS"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("49", 5, 13, "SS"),
         )
         val lowerDeckSleeperSeatsList = listOf(
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L1", 1, 1),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L4", 1, 3),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L7", 1, 5),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L10", 1, 7),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L13", 1, 9),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L16", 1, 11),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L2", 3, 1),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L5", 3, 3),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L8", 3, 5),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L11", 3, 7),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L14", 3, 9),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L17", 3, 11),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L3", 4, 1),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L6", 4, 3),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L9", 4, 5),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L12", 4, 7),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L15", 4, 9),
-            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L18", 4, 11),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L1", 1, 1, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L4", 1, 3, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L7", 1, 5, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L10", 1, 7, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L13", 1, 9, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L16", 1, 11, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L2", 3, 1, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L5", 3, 3, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L8", 3, 5, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L11", 3, 7, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L14", 3, 9, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L17", 3, 11, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L3", 4, 1, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L6", 4, 3, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L9", 4, 5, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L12", 4, 7, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L15", 4, 9, "LB"),
+            BusLayoutInfo.TotalSeatList.LowerdeckSeatNo("L18", 4, 11, "LB"),
         )
 
 
@@ -157,11 +158,11 @@ class BusSeatMapActivity : AppCompatActivity() {
             var cachedPreviousSeatType: String? = null
             val timeTaken = measureTime {
 
-                addEmptyCellsToGrid(totalColsSleeperCumSeater, totalRowsSleeperCumSeater)
+//                addEmptyCellsToGrid(totalColsSleeperCumSeater, totalRowsSleeperCumSeater)
 
                 drawSeatMap(
-                    dividerRowSleeperCumSeater,
-                    lowerDeckSleeperCumSeaterList
+                    dividerRow,
+                    lowerDeckSleeperSeatsList
                 )
 
 //                for (i in 1..totalRowsSleeperCumSeater) {
@@ -268,7 +269,7 @@ class BusSeatMapActivity : AppCompatActivity() {
             for (c in 0 until glSeatMap.columnCount) {
                 val layoutParams = GridLayout.LayoutParams()
                 layoutParams.apply {
-                    rowSpec = GridLayout.spec(0, 0, 1f)
+                    rowSpec = GridLayout.spec(1, 1, 1f)
                     columnSpec = GridLayout.spec(c, 1, 0f)
                     setGravity(Gravity.FILL)
                     width = 0
@@ -280,7 +281,7 @@ class BusSeatMapActivity : AppCompatActivity() {
                 val layoutParams = GridLayout.LayoutParams()
                 layoutParams.apply {
                     rowSpec = GridLayout.spec(r, 1, 0f)
-                    columnSpec = GridLayout.spec(0, 0, 0f)
+                    columnSpec = GridLayout.spec(1, 1, 0f)
                     setGravity(Gravity.FILL)
                     width = 0
                     glSeatMap.addView(Space(this@BusSeatMapActivity), this)
@@ -293,10 +294,12 @@ class BusSeatMapActivity : AppCompatActivity() {
 
     private fun drawSeatMap(
         dividerRowSleeperCumSeater: Int,
-        lowerDeckSleeperCumSeaterList: List<BusLayoutInfo.TotalSeatList.LowerdeckSeatNo>
+        lowerDeckSleeperCumSeaterList: List<BusLayoutInfo.TotalSeatList.LowerdeckSeatNo>,
     ) {
         busSeatMapBinding?.run {
             glSeatMap.apply {
+                rowCount = 11
+                columnCount = 4
                 for (i in 1..columnCount) {
                     val isDividerRow =
                         lowerDeckSleeperCumSeaterList.firstOrNull { dividerRowSleeperCumSeater == i }
@@ -319,29 +322,26 @@ class BusSeatMapActivity : AppCompatActivity() {
                             val seaterSeatLayoutBinding = BusSeatLayoutBinding.inflate(
                                 layoutInflater, root, false
                             )
-                            val layoutParams =
-                                seaterSeatLayoutBinding.rlBusSeater.layoutParams as MarginLayoutParams
-                            layoutParams.setMargins(dpToPx(8))
-                            seaterSeatLayoutBinding.rlBusSeater.layoutParams = layoutParams
                             seaterSeatLayoutBinding.root
 
                         } else if (seatInfo?.Availability == "LB") {
                             val sleeperSeatLayoutBinding = SleeperSeatLayoutBinding.inflate(
                                 layoutInflater, root, false
                             )
-
-                            val layoutParams =
-                                sleeperSeatLayoutBinding.rlBusSleeper.layoutParams as MarginLayoutParams
-                            layoutParams.setMargins(dpToPx(8))
-                            sleeperSeatLayoutBinding.rlBusSleeper.layoutParams = layoutParams
                             sleeperSeatLayoutBinding.root
                         } else {
-                            Timber.e("empty seat at row $i and at col $j and seat type is SS")
-                            val seaterSeatLayoutBinding = BusSeatLayoutBinding.inflate(
-                                layoutInflater, root, false
-                            )
-                            seaterSeatLayoutBinding.rlBusSeater.visibility = INVISIBLE
-                            seaterSeatLayoutBinding.root
+                            null
+//                            if(isDividerRow){
+//                            val seaterSeatLayoutBinding = BusSeatLayoutBinding.inflate(
+//                                layoutInflater, root, false
+//                            )
+//                            seaterSeatLayoutBinding.rlBusSeater.visibility = INVISIBLE
+//                            seaterSeatLayoutBinding.root
+//                            }else{
+//                                null
+//                            }
+
+
 //                            if (isDividerRow) {
 //                                Timber.e("empty seat at row $i and at col $j and seat type is SS")
 //                                val seaterSeatLayoutBinding = BusSeatLayoutBinding.inflate(
@@ -365,13 +365,14 @@ class BusSeatMapActivity : AppCompatActivity() {
                         }
 
                         val gridLayoutParams = GridLayout.LayoutParams(
-                            GridLayout.spec(rowIndex,if(seatInfo?.Availability == "LB")0 else 1),
-                            GridLayout.spec(colIndex,1),
+                            GridLayout.spec(rowIndex, if (seatInfo?.Availability == "LB") 1 else 1),
+                            GridLayout.spec(colIndex, 1),
                         )
+
 
                         Timber.e("rowindex is $rowIndex and colIndex is $colIndex and seat type is ${seatInfo?.Availability}")
 
-                        view.let { addView(it, gridLayoutParams) }
+                        view?.let { addView(it, gridLayoutParams.apply { setMargins(dpToPx(8)) }) }
                     }
                 }
             }
